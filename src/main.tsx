26106aa89089a0155665452c24f47308c637ef2d
PARTH BIDARI <bidarip@science.regn.net>
2025-09-15 15:05:20 +0530

    added Graphene Root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx';
+import { GrapheneRoot } from '@elsevier/graphene';
+// import { GrapheneProvider } from '@elsevier/graphene';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <GrapheneRoot>
+      <App />
+    </GrapheneRoot>
+  </StrictMode>
 )
